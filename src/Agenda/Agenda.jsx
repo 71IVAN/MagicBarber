0494372fc11384f62b8267df1a278459a5ef8491
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-04-28 11:37:44 -0500

    se agregan cambios

--- src/Agenda/Agenda.jsx
+++ src/Agenda/Agenda.jsx
@@ -1,60 +1,72 @@
-export function Agenda(){
+import './Agenda.css'
+import WOW from 'wow.js'
+import 'animate.css'
+import { useEffect } from 'react'
+export function Agenda() {
 
-    return(
-        <>
-              <form >
-        <div className="row">
-          <div className="col-12">
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">Nombre Rodriguez</span>
-              <input type="text" class="form-control" placeholder="Julian" aria-label="Username" aria-describedby="basic-addon1" />
+  useEffect(function () {
+    const wow = new WOW()
 
-            </div>
-          </div>
+  }, [])
 
-
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">Correo</span>
-              <input type="email" class="form-control" placeholder="julian99@gmail.com" aria-label="Username" aria-describedby="basic-addon1" />
-            </div>
-          </div>
-
-          <div className="col-6">
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">Celular</span>
-              <input type="number" class="form-control" placeholder="3007890044" aria-label="Username" aria-describedby="basic-addon1"/>
-            </div>
-          </div>
+  return (
 
 
-        </div>
-
-
+    <>
    
-      <div className="row">
-      <div className="col-6">
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">Fecha</span>
-              <input type="number" class="form-control" placeholder="3007890044" aria-label="Username" aria-describedby="basic-addon1"/>
+       <div className="banner p-5">
+      <form className='my-5'>
+      <h3 className='wow animate__animated animate__bounceInDown'>RESERVA TU TURNO</h3>
+
+        <div className="row">
+          <div className="col-3 col-md-6 ">
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1"><i className="bi bi-person-badge-fill"></i> </span>
+              <input type="text" className="form-control" placeholder="Julian Rodriguez" />
             </div>
           </div>
-
-      </div>
-
-      <div className="row">
-      <div className="col-6">
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">Hora</span>
-              <input type="number" class="form-control" placeholder="3007890044" aria-label="Username" aria-describedby="basic-addon1"/>
+          <div className="col-3 col-md-6">
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1"><i className="bi bi-envelope-fill"></i></span>
+              <input type="text" className="form-control" placeholder="jualian88@gmail.com" />
             </div>
           </div>
+        </div>
 
+        <div className="row">
+          <div className="col-3 col-md-6">
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1"><i class="bi bi-calendar2-date-fill"></i></span>
+              <input type="date" className="form-control" placeholder="agenda" />
+            </div>
+          </div>
+          <div className="col-3 col-md-6">
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1"><i className="bi bi-clock-history"></i></span>
+              <select className="form-select" defaultValue={'DEFAULT'}>
+                <option value="DEFAULT">Hora:</option>
+                <option value="1">6:30 am</option>
+                <option value="2">7:00 am</option>
+                <option value="3">7:30 am</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6">
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1"><i className="bi bi-phone-fill"></i></span>
+              <input type="text" className="form-control" placeholder="3004509977" />
+            </div>
+
+          </div>
+        </div>
+
+
+
+      </form>
       </div>
-
-    </form >
-        </>
-    )
+    </>
+  )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { Agenda } from './Agenda/Agenda'
 ReactDOM.createRoot(document.getElementById('root')).render(
